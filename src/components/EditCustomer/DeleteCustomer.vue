d66cdba87ab70a6caad2ca394dfe1d5eae50da5b
Fu <isla17092002@gmail.com>
2024-06-22 01:18:51 +0700

    update new code

--- src/components/EditCustomer/DeleteCustomer.vue
+++ src/components/EditCustomer/DeleteCustomer.vue
@@ -1,62 +1,64 @@
 <template>
-  <!-- Button trigger modal -->
-  <button
-    type="button"
-    class="btn btn-danger"
-    data-bs-toggle="modal"
-    data-bs-target="#DeleteCustomer"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-dash-circle"
-      viewBox="0 0 16 16"
-    >
-      <path
-        d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
-      />
-      <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
-    </svg>
-    Xóa
-  </button>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-danger" data-bs-toggle="modal" :data-bs-target="`#${id}`">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-dash-circle"
+            viewBox="0 0 16 16"
+        >
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+            <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8" />
+        </svg>
+        Xóa
+    </button>
 
-  <!-- Modal -->
-  <div
-    class="modal fade"
-    id="DeleteCustomer"
-    data-bs-backdrop="static"
-    data-bs-keyboard="false"
-    tabindex="-1"
-    aria-labelledby="staticBackdropLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="staticBackdropLabel">
-            Xóa tài khoản người dùng
-          </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+    <!-- Modal -->
+    <div
+        class="modal fade"
+        :id="id"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Xóa tài khoản người dùng</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">Bạn chắc chắn muốn xóa tài khoản này chứ ?</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                    <button type="button" class="btn btn-danger" @click="deleteCustomer">
+                        <span v-if="loading">Xóa...</span>
+                        <span v-else>Xóa</span>
+                    </button>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">Bạn chắc chắn muốn xóa tài khoản này chứ ?</div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Hủy
-          </button>
-          <button type="button" class="btn btn-danger">Xóa</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
+
+<script setup>
+import { db } from "@/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const props = defineProps({
+    id: { type: String },
+});
+const loading = ref(false);
+const router = useRouter();
+const deleteCustomer = async (id) => {
+    loading.value = true;
+    await deleteDoc(doc(db, "users", props.id));
+    loading.value = false;
+    router.go(0);
+};
+</script>
--- src/components/EditCustomer/FixCustomer.vue
+++ src/components/EditCustomer/FixCustomer.vue
@@ -1,140 +1,123 @@
 <template>
-  <button
-    type="button"
-    class="btn btn-success m-1"
-    data-bs-toggle="modal"
-    data-bs-target="#FixCustomer"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      aria-hidden="true"
-      role="img"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      class="iconify iconify--lucide"
-      style="color: white"
-    >
-      <g
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-      >
-        <path
-          d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"
-        ></path>
-        <path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1l1-4Z"></path>
-      </g>
-    </svg>
-    Sửa
-  </button>
-  <div
-    class="modal fade"
-    id="FixCustomer"
-    tabindex="-1"
-    aria-labelledby="exampleModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">
-            Sửa tài khoản người dùng
-          </h1>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <form>
-            <div class="mb-3">
-              <label for="recipient-name" class="col-form-label"
-                >Tên sản phẩm:</label
-              >
-              <input type="text" class="form-control" id="recipient-name" />
-            </div>
-            <div class="mb-3">
-              <label for="brand" class="form-label">Hãng:</label
-              ><select
-                class="form-select form-select-sm mb-3"
-                aria-label="Large select example"
-                fdprocessedid="kijs1k"
-              >
-                <option value="Tide">Apple</option>
-                <option value="Gain">SamSung</option>
-                <option value="Persil">Xiaomi</option>
-                <option value="Persil">Oppo</option>
-              </select>
-            </div>
-            <div class="mb-3">
-              <label for="message-text" class="col-form-label">Mô tả:</label>
-              <textarea class="form-control" id="message-text"></textarea>
-            </div>
-            <div class="mb-3">
-              <label for="quantity" class="form-label">Tồn kho:</label
-              ><input
-                type="number"
-                class="form-control"
-                name="quantity"
-                id="quantity"
-                aria-describedby="quantityID"
-                fdprocessedid="w0mucc"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="quantity" class="form-label">Giá sản phẩm:</label
-              ><input
-                type="number"
-                class="form-control"
-                name="quantity"
-                id="quantity"
-                aria-describedby="quantityID"
-                fdprocessedid="w0mucc"
-              />
-            </div>
-            <div class="mb-3">
-              <label for="imageUrl" class="form-label">Chọn ảnh</label
-              ><input
-                type="file"
-                class="form-control"
-                accept="image/*"
-                name="imageUrl"
-                id="imageUrl"
-                placeholder=""
-                aria-describedby="fileHelpId"
-              />
+    <button type="button" class="btn btn-success m-1" data-bs-toggle="modal" :data-bs-target="`#${index}`">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            class="iconify iconify--lucide"
+            style="color: white"
+        >
+            <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                <path d="M18.375 2.625a2.121 2.121 0 1 1 3 3L12 15l-4 1l1-4Z"></path>
+            </g>
+        </svg>
+        Sửa
+    </button>
+    <div class="modal fade" :id="index" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Sửa tài khoản người dùng</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form @submit.prevent="updateUser">
+                    <div class="modal-body">
+                        <!--v-if-->
+                        <div class="mb-3">
+                            <!-- Tên người dùng --><label for="username" class="form-label">Tên người dùng</label
+                            ><input
+                                type="text"
+                                name="username"
+                                id="username"
+                                class="form-control"
+                                placeholder=""
+                                v-model="userEdit.username"
+                            />
+                        </div>
+                        <!-- Email -->
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label
+                            ><input
+                                type="email"
+                                name="email"
+                                id="email"
+                                class="form-control"
+                                placeholder=""
+                                v-model="userEdit.email"
+                            />
+                        </div>
+                        <!-- Mật khẩu -->
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Mật khẩu</label
+                            ><input
+                                type="password"
+                                name="password"
+                                id="password"
+                                class="form-control"
+                                placeholder=""
+                                v-model="userEdit.password"
+                            />
+                        </div>
+                        <!-- Chọn quyền  -->
+                        <div class="mb-3">
+                            <label for="role" class="form-label">Quyền</label
+                            ><select class="form-select form-select-sm" name="role" id="role" v-model="userEdit.role">
+                                <option>Chọn quyền</option>
+                                <option value="ADMIN">ADMIN</option>
+                                <option value="USER">USER</option>
+                            </select>
+                        </div>
+                    </div>
 
-              <div class="mb-3">
-                <input
-                  type="text"
-                  class="form-control"
-                  name=""
-                  id=""
-                  aria-describedby="helpId"
-                  placeholder=""
-                  fdprocessedid="xohzq"
-                />
-              </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+                        <button type="submit" class="btn btn-success">
+                            <span v-if="loading">Đang thêm...</span>
+                            <span v-else>Xác nhận</span>
+                        </button>
+                    </div>
+                </form>
             </div>
-          </form>
         </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Hủy
-          </button>
-          <button type="button" class="btn btn-primary">Lưu</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
+
+<script setup>
+import { db } from "@/firebase";
+import { doc, updateDoc } from "firebase/firestore";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { toast } from "vue3-toastify";
+
+const props = defineProps({
+    data: { type: Object },
+    index: {
+        type: Number,
+    },
+});
+const loading = ref(false);
+const userEdit = ref({
+    id: props.data.id,
+    username: props.data.username,
+    email: props.data.email,
+    password: props.data.password,
+    role: props.data.role,
+});
+const router = useRouter();
+const updateUser = async () => {
+    loading.value = true;
+    await updateDoc(doc(db, "users", `${userEdit.value.id}`), {
+        ...userEdit.value,
+    });
+    loading.value = false;
+
+    toast.success("Sửa thành công");
+
+    router.go(0);
+};
+</script>
